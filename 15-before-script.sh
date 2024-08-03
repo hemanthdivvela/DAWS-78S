fdeb329b43ae3a562dd52f1ac74f69a8f3718025
hemanthdivvela <divvelahemanth7@gmail.com>
2024-08-03 17:18:50 +0530

    newcode

--- 15-before-script.sh
+++ 15-before-script.sh
@@ -4,5 +4,6 @@ COURSE="dEVOPS FROM CURRENT SCRIPT"
 echo "Before calling other script, COURSE: $COURSE"
 echo "Process id of current shell-script: $$"
 
-./16-after-script.sh
+#./16-after-script.sh
+source ./16-after-script.sh
 echo "Before calling other script, COURSE: $COURSE"
