138eec39cf7c3cd7be7e7578f4af60daa2e877c9
hemanthdivvela <divvelahemanth7@gmail.com>
2024-08-05 18:13:46 +0530

    update

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+if  [-d $SOURCE_DIRECTORY ]
+then 
+    echo -e "$G source directory exists $N"
+else
+    echo " $R please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
+fi
