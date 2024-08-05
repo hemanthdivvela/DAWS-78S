252d269830e2cbd2160692c56bf38231e4f75ac8
hemanthdivvela <divvelahemanth7@gmail.com>
2024-08-05 18:42:24 +0530

    update

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -8,10 +8,13 @@ N="\e[0m"
 Y="\e[33m"
 
 
-if  [-d $SOURCE_DIRECTORY ]
+if  [ -d $SOURCE_DIRECTORY ]
 then 
     echo -e "$G source directory exists $N"
 else
     echo -e "$R please make sure $SOURCE_DIRECTORY exists $N"
     exit 1
 fi
+
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+echo "Files to delete: $FILES"
